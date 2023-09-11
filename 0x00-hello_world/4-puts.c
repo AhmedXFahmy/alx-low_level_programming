8bc1a9826f54d6f0806ff935741369868107c301
root <root@MATEBOOK.localdomain>
2023-09-11 20:38:53 +0300

    new update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - int is equall to integer and its value is void
- * return : alwayes the return is equal to 0
+ * Return: Alwayes 0 (Success)
  */
 int main(void)
 {
