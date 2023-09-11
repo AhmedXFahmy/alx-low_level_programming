52b5415e6a6e80f9f11d1b61253b86c6352247af
root <root@MATEBOOK.localdomain>
2023-09-11 20:34:17 +0300

    new update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-#!/ bin / bash
 #include <stdio.h>
+/**
+ * main - int is equall to integer and its value is void
+ * return : alwayes the return is equal to 0
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-GG=g
