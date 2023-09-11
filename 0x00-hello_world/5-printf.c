1e40e1f557aec3214a2e6b9139eea53c9f29ed89
root <root@MATEBOOK.localdomain>
2023-09-11 20:41:14 +0300

    new update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - int is equall to integer and its value is void
+ * Return: Alwayes 0 (Success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
